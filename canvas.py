0386f1cd9ad6be40d0b55ed997033f1bb307c296
unknown <xunan.leslie@gmail.com>
2019-02-17 02:39:42 -0500

    Create Canvas class.

---
+++ canvas.py
@@ -0,0 +1,29 @@
+class Canvas():
+
+    def __init__(self, width, height, color):
+
+        break
+
+    def set_width(self, w):
+
+        break
+
+    def set_height(self,h):
+
+        break
+    
+    def set_color(self, c):
+
+        break
+
+    def get_width(self):
+
+        break
+    
+    def get_height(self):
+
+        break
+
+    def draw():
+
+        break
